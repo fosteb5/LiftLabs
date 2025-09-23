fe9c4082a196ebd27364c34e886cb0dee78811c1
Foster Beard <foster5beard@gmail.com>
2025-09-23 13:40:55 -0400

    working ultrasonic library code

--- ultrasonicLibraryTesting/ultrasonicLibraryTesting.ino
+++ ultrasonicLibraryTesting/ultrasonicLibraryTesting.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Ultrasonic.h>
 
+// Define pins
+#define TRIG_PIN 15
+#define ECHO_PIN 2
+
+// Create ultrasonic object
+Ultrasonic ultrasonic(TRIG_PIN, ECHO_PIN);
+
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Get distance in centimeters
+  long distanceCM = ultrasonic.read();
 
+  Serial.print("Distance: ");
+  Serial.print(distanceCM);
+  Serial.println(" cm");
+
+  delay(500); // wait half a second
 }

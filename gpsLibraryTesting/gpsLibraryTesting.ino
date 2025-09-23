6074e4fbe179196a7903ee42800ae6b727a5be3f
Foster Beard <foster5beard@gmail.com>
2025-09-23 15:04:03 -0400

    not tested yet

--- gpsLibraryTesting/gpsLibraryTesting.ino
+++ gpsLibraryTesting/gpsLibraryTesting.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
 
+TinyGPSPlus gps;
+HardwareSerial SerialGPS(2);  // use UART2 (pins 16 RX, 17 TX)
+
+void setup() {
+  Serial.begin(115200); // debug via USB
+  SerialGPS.begin(9600, SERIAL_8N1, 16, 17); // GPS: RX=16, TX=17
+  Serial.println("GPS starting...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  while (SerialGPS.available() > 0) {
+    gps.encode(SerialGPS.read());
+    if (gps.location.isUpdated()) {
+      Serial.print("Lat: "); Serial.println(gps.location.lat(), 6);
+      Serial.print("Lng: "); Serial.println(gps.location.lng(), 6);
+    }
+  }
 }

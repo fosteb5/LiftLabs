e2457406ef7849bf36d71d29b7fed1c4252efa72
Foster Beard <foster5beard@gmail.com>
2025-11-18 14:48:51 -0500

    the main code for the blimp

---
+++ mainCode/mainCode.ino
@@ -0,0 +1,10 @@
+// this will be the new and complete code for the blimp, this will encompass all of the libs
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

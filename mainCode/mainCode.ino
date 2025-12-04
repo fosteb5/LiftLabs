b5c03b73690f9eb9cb7ce33fd7cc7c64d2a797c1
Foster Beard <foster5beard@gmail.com>
2025-12-04 15:05:29 -0500

    starting creating varibles and functions for the sensors

--- mainCode/mainCode.ino
+++ mainCode/mainCode.ino
@@ -1,4 +1,29 @@
 // this will be the new and complete code for the blimp, this will encompass all of the libs
+// this will import all off the libraries that are needed for the code
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP3XX.h>
+#include <HardwareSerial.h>
+#include <TinyGPSPlus.h> // used to make the gps function
+#include <Adafruit_MPU6050.h>
+#include <ESP32Servo.h> // used to make the servo function
+#include <Ultrasonic.h> // needed to calculate distannce with the ultrasonic
+ 
+ // establishing the GPS module
+ HardwareSerial GPS_serial(1); // setting the serial to read
+ static const int GPS_RX_PIN = 16; // RX pin for the gps
+ static const int GPS_TX_PIN = 17; // TX pin for the gps
+
+// establishing the gyroscope module
+Adafruit_MPU6050 mpu;
+float pitch = 0, roll = 0; // setting floats to hold the pitch and roll of the blimp/module
+
+// establishing the servo's
+Servo myServo;
+static const int servoLeftMotor = 18;
+int min = 500; // in micro seconds
+int max = 2500; // in micro seconds
+ 
 void setup() {
   // put your setup code here, to run once:
 
